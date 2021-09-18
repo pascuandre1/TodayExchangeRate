b4aefa073b00aa6d55a68528060b1f58ff7455cb
Andrei Pascu <Andrei_Pascu@waters.com>
2021-09-18 17:03:07 +0300

    add configuration option for output

--- TodayExchangeRate/TodayExchangeRate/Program.cs
+++ TodayExchangeRate/TodayExchangeRate/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace TodayExchangeRate
 {
     class Program
     {
+        private static string PrintMode = ConfigurationManager.AppSettings["PrintMode"];
         static void Main(string[] args)
         {
+            switch (PrintMode)
+            {
+                case "Console":
+                    Console.WriteLine("Console mode");
+                    Console.ReadLine();
+                    break;
+                case "MessageBox":
+                    MessageBox.Show("MessageBox mode");
+                    break;
+                default:
+                    Console.WriteLine("Default console mode");
+                    Console.ReadLine();
+                    break;
+            }
+
         }
     }
 }
