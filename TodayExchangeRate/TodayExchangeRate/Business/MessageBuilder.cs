5cae0ee72821553c6650cd9c7ba7404e7e67ac55
Andrei Pascu <Andrei_Pascu@waters.com>
2021-09-18 18:09:44 +0300

    up and running

---
+++ TodayExchangeRate/TodayExchangeRate/Business/MessageBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodayExchangeRate.Business
+{
+    public class MessageBuilder
+    {
+        public string BuildExhangeMessage(decimal apiRate, decimal marketRate)
+        {
+            string additionalMessage = "";
+            string exchangeMessageBase =
+                $"Exchange rate from api layer: {apiRate.ToString(CultureInfo.InvariantCulture)}" +
+                $" \n Exchange rate from open market: {marketRate.ToString(CultureInfo.InvariantCulture)} \n";
+
+
+            if (apiRate > marketRate)
+                additionalMessage = "You should exchange from api rate";
+            if (apiRate == marketRate)
+                additionalMessage = "The rates are the same, you can exchange from any market";
+            if (apiRate < marketRate)
+                additionalMessage = "You should exchange from open market rate";
+            return exchangeMessageBase + additionalMessage;
+        }
+    }
+}
--- TodayExchangeRate/TodayExchangeRate/Program.cs
+++ TodayExchangeRate/TodayExchangeRate/Program.cs
@@ -1,29 +1,54 @@
 ﻿using System;
 using System.Configuration;
 using System.Windows.Forms;
+using TodayExchangeRate.Business;
 
 namespace TodayExchangeRate
 {
     class Program
     {
         private static string PrintMode = ConfigurationManager.AppSettings["PrintMode"];
+        private static ExchangeRates exchangeRates = new ExchangeRates();
+        private static MessageBuilder messageBuilder = new MessageBuilder();
 
         static void Main(string[] args)
         {
+            var apiRate = GetApiLayerExchangeRate();
+            var openMarketRate = GetOpenMarketExchangeRate();
+            var message = messageBuilder.BuildExhangeMessage(apiRate, openMarketRate);
+
             switch (PrintMode)
             {
                 case "Console":
-                    Console.WriteLine("Console mode");
+                    Console.WriteLine(message);
                     Console.ReadLine();
                     break;
                 case "MessageBox":
-                    MessageBox.Show("MessageBox mode");
+                    MessageBox.Show(message);
                     break;
                 default:
-                    Console.WriteLine("Default console mode");
+                    Console.WriteLine(message);
                     Console.ReadLine();
                     break;
             }
         }
+
+        #region private methods
+
+        private static decimal GetApiLayerExchangeRate()
+        {
+            var apiExchangeResult = exchangeRates.GetApiLayerRatesAsync().Result;
+            var usdEurRate = apiExchangeResult.Quotes["USDEUR"];
+            return usdEurRate;
+        }
+
+        private static decimal GetOpenMarketExchangeRate()
+        {
+            var openMarketResult = exchangeRates.GetOpenMarketRatesAsync().Result;
+            var euroRate = openMarketResult.Rates["EUR"];
+            return euroRate;
+        }
+
+        #endregion private methods
     }
 }
